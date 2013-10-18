62382acaf4ebb8283c88d3d76ff6c7af4a4d3a46
Harry McIntyre <mcintyre321@gmail.com>
2013-10-18 08:19:26 +0100

    Fixed assembly name

--- ChessEngine/Properties/AssemblyInfo.cs
+++ ChessEngine/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChessEngine.Portable")]
+[assembly: AssemblyTitle("ChessEngine")]
 [assembly: AssemblyDescription("The rules of chess as a portable library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChessEngine.Portable")]
+[assembly: AssemblyProduct("ChessEngine")]
 [assembly: AssemblyCopyright("Copyright © Harry McIntyre 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("ChessEngine.Tests")]
 
